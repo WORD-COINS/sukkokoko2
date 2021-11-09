3465fec31f9c0d309f6f83f0a34043fc4f26110c
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2021-11-10 01:15:38 +0900

    fixup! Fix aggregation algorithm

--- src/chat-speed-aggregation.ts
+++ src/chat-speed-aggregation.ts
@@ -8,16 +8,28 @@ const getNumberOfDayPost = async (client: WebClient, channel: ChannelID) => {
   const yesterday = (() => {
     const yesterday = new Date();
     yesterday.setDate(yesterday.getDate() - 1);
-    yesterday.setHours(0)
-    yesterday.setSeconds(0)
-    yesterday.setMilliseconds(0)
+    yesterday.setHours(0);
+    yesterday.setMinutes(0);
+    yesterday.setSeconds(0);
+    yesterday.setMilliseconds(0);
     return yesterday;
   })();
   const oldest = (yesterday.getTime() / 1000).toString();
 
+  const today = (() => {
+    const today = new Date();
+    today.setHours(0);
+    today.setMinutes(0);
+    today.setSeconds(0);
+    today.setMilliseconds(0);
+    return today;
+  })();
+  const latest = (today.getTime() / 1000).toString();
+
   const result = await client.conversations.history({
     channel,
     oldest,
+    latest,
     limit: 500,
   });
 
