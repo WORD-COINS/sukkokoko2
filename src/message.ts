d6b59b04b9e08ee93658f304ee5582b0708e0040
azarashi2931 <46126763+azarashi2931@users.noreply.github.com>
2021-11-09 19:36:06 +0900

    Remove message line head comma

--- src/message.ts
+++ src/message.ts
@@ -3,6 +3,6 @@ import { ChatSpeedAggregationResult } from "./types";
 export const buildMessage = (results: ChatSpeedAggregationResult[]) => {
   return (
     "*⏱本日の 流速強さ ランキング (575)🏃‍♂️🏃‍♂️🏃‍♂️*\n" +
-    results.map((result) => `<#${result.channel}>:\t${result.numberOfPost}\n`)
+    results.map((result) => `<#${result.channel}>:\t${result.numberOfPost}\n`).join("")
   );
 };
